e445a6b1227fb54f17fd8721172fc969c4e29d33
chocoluffy <yushunzhe951104@gmail.com>
2018-12-01 14:31:49 -0500

    Add luffy plsr submission.

--- luffy/PLSR/nn.py
+++ luffy/PLSR/nn.py
@@ -24,6 +24,32 @@ import pandas as pd
 from sklearn.cross_decomposition import PLSRegression
 import os.path
 
+import tensorflow as tf
+import keras.backend.tensorflow_backend as tfb
+
+POS_WEIGHT = 10  # multiplier for positive targets, needs to be tuned
+
+def weighted_binary_crossentropy(target, output):
+    """
+    Weighted binary crossentropy between an output tensor 
+    and a target tensor. POS_WEIGHT is used as a multiplier 
+    for the positive targets.
+
+    Combination of the following functions:
+    * keras.losses.binary_crossentropy
+    * keras.backend.tensorflow_backend.binary_crossentropy
+    * tf.nn.weighted_cross_entropy_with_logits
+    """
+    # transform back to logits
+    _epsilon = tfb._to_tensor(tfb.epsilon(), output.dtype.base_dtype)
+    output = tf.clip_by_value(output, _epsilon, 1 - _epsilon)
+    output = tf.log(output / (1 - output))
+    # compute weighted loss
+    loss = tf.nn.weighted_cross_entropy_with_logits(targets=target,
+                                                    logits=output,
+                                                    pos_weight=POS_WEIGHT)
+    return tf.reduce_mean(loss, axis=-1)
+
 """
 BOW_all use PCA.
 """
@@ -33,18 +59,22 @@ test_derscription_feature_map = pickle.load(open('./features/11_30_[test]_descri
 train_bow_lst = list(map(lambda x: x["BOW_all_normalized_vector"], train_description_feature_map))
 test_bow_lst = list(map(lambda x: x["BOW_all_normalized_vector"], test_derscription_feature_map))
 
-pca_model_name = "./models/pca_bow_all_to_512.pkl"
-if os.path.exists(pca_model_name):
-    pca = pickle.load(open(pca_model_name, 'rb'))
-else:
-    pca = PCA(n_components=512)
-    pca.fit(train_bow_lst)
-    pickle.dump(pca, open(pca_model_name, 'wb'), protocol=2)
+train_bow_lst = np.asarray(train_bow_lst)
+test_bow_lst = np.asarray(test_bow_lst)
+print(train_bow_lst.shape)
 
-des_BOW_all_train_pca = pca.transform(train_bow_lst)
-des_BOW_all_test_pca = pca.transform(test_bow_lst)
+# pca_model_name = "./models/pca_bow_all_to_512.pkl"
+# if os.path.exists(pca_model_name):
+#     pca = pickle.load(open(pca_model_name, 'rb'))
+# else:
+#     pca = PCA(n_components=512)
+#     pca.fit(train_bow_lst)
+#     pickle.dump(pca, open(pca_model_name, 'wb'), protocol=2)
 
-print(des_BOW_all_test_pca.shape)
+# des_BOW_all_train_pca = pca.transform(train_bow_lst)
+# des_BOW_all_test_pca = pca.transform(test_bow_lst)
+
+# print(des_BOW_all_test_pca.shape)
 
 """
 Training data.
@@ -62,25 +92,26 @@ input: pool5:2048
 output: BOW_all_pca: 512
 """
 input_size = 2048
-output_size = 512
+output_size = train_bow_lst.shape[1]
 
 model = Sequential()
-model.add(Dropout(0.3))
-model.add(Dense(input_size, 1600, activation='relu'))
+model.add(Dense(input_dim=input_size, output_dim=2048, activation='relu'))
 model.add(Dropout(0.6))
-model.add(Dense(1600, 1200, activation='relu'))
+model.add(Dense(input_dim=2048, output_dim=2500, activation='relu'))
 model.add(Dropout(0.6))
-model.add(Dense(1200, 800, activation='relu'))
+model.add(Dense(input_dim=2500, output_dim=3036, activation='relu'))
 model.add(Dropout(0.6))
-model.add(Dense(800, output_size, activation='sigmoid'))
+model.add(Dense(input_dim=3036, output_dim=4096, activation='relu'))
+model.add(Dropout(0.6))
+model.add(Dense(input_dim=4096, output_dim=output_size, activation='sigmoid'))
 
-model.compile(optimizer=Adam(), loss='binary_crossentropy')
+model.compile(optimizer=Adam(), loss=weighted_binary_crossentropy)
 
 model_checkpoint = ModelCheckpoint('./models/' + 'weights_12_1_epoch_{epoch:02d}.h5', monitor='val_loss', save_best_only=True)
 
 # model.fit(xt, yt, batch_size=64, nb_epoch=500, validation_data=(xs, ys), class_weight=W, verbose=0)
 
-model.fit(train_pool5_img, des_BOW_all_train_pca, batch_size=64, nb_epoch=500, verbose=1, shuffle=True,
+model.fit(train_pool5_img, train_bow_lst, batch_size=64, nb_epoch=500, verbose=1, shuffle=True,
             validation_split=0.1,
             callbacks=[model_checkpoint])
 
--- luffy/PLSR/plsr.py
+++ luffy/PLSR/plsr.py
@@ -12,11 +12,11 @@ test_derscription_feature_map = pickle.load(open('./features/11_30_[test]_descri
 train_bow_lst = list(map(lambda x: x["BOW_all_normalized_vector"], train_description_feature_map))
 test_bow_lst = list(map(lambda x: x["BOW_all_normalized_vector"], test_derscription_feature_map))
 
-pca_model_name = "./models/pca_bow_all_to_3500.pkl"
+pca_model_name = "./models/pca_bow_all_to_1024.pkl"
 if os.path.exists(pca_model_name):
     pca = pickle.load(open(pca_model_name, 'rb'))
 else:
-    pca = PCA(n_components=3500)
+    pca = PCA(n_components=1024)
     pca.fit(train_bow_lst)
     pickle.dump(pca, open(pca_model_name, 'wb'), protocol=2)
 
@@ -31,8 +31,8 @@ test_img_map = pickle.load(open('./features/12_1_test_img_feature_map_py27.pkl',
 train_pool5_img = np.asarray(list(map(lambda x: x["POOL_vector"], train_img_map)))
 test_pool5_img = np.asarray(list(map(lambda x: x["POOL_vector"], test_img_map)))
 
-pls_pool5_to_BOW_all_pca_3500 = PLSRegression(n_components=800)
-pls_pool5_to_BOW_all_pca_3500.fit(train_pool5_img[:8000], des_BOW_all_train_pca[:8000])
-model_name = "./models/pls_pool5_to_BOW_all_pca_3500.pkl"
-pickle.dump(pls_pool5_to_BOW_all_pca_3500, open(model_name, 'wb'), protocol=2)
-print pls_pool5_to_BOW_all_pca_3500.score(train_pool5_img[8000:], des_BOW_all_train_pca[8000:])
+pls_pool5_to_BOW_all_pca_1024 = PLSRegression(n_components=600)
+pls_pool5_to_BOW_all_pca_1024.fit(train_pool5_img[:8000], des_BOW_all_train_pca[:8000])
+model_name = "./models/pls_600_pool5_to_BOW_all_pca_1024.pkl"
+pickle.dump(pls_pool5_to_BOW_all_pca_1024, open(model_name, 'wb'), protocol=2)
+print pls_pool5_to_BOW_all_pca_1024.score(train_pool5_img[8000:], des_BOW_all_train_pca[8000:])
